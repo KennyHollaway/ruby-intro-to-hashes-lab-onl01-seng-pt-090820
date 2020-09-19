1a400bdbf55c52d1920b038753b1f1eeec2045f8
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-19 03:02:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,6 @@ end
 
 def monopoly_with_second_tier
   monopoly = {
-  # you built in the previous method. Run the tests. They will fail, but	    :railroads => 
-  # they'll guide you in how to modify what you just did to have more	    {
-  # complexity.	      :pieces => 4 
-    }
+ 	    :railroads =>  {pieces => 4 }
   }
+end
